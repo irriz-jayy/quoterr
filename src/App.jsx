9b89f11725db6ce1944d30b84b12a0f5137b779e
irriz-jayy <jayfelix789@gmail.com>
2023-07-25 23:36:22 +0300

    completed navbar and welcome section

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,34 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Card from "./assets/components/Card";
+// import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <header>
+      <div className="min-h-screen bg-main1 font-main">
+        <header className="flex items-center justify-between drop-shadow-lg h-12 px-4 bg-main1 font-logo">
+          <div>
+            <p className="text-text1 text-2xl">
+              <span className="text-logoQ font-bold">Q</span>uoterr
+            </p>
+          </div>
+          <div>Toggler</div>
+        </header>
+        <div className="flex flex-col items-center min-h-48 justify-center">
+          <p className="text-xl">
+            Welcome to <span className="font-logo">Quoterr</span>
+          </p>
+          <p className="text-center text-base">
+            Here Inspiration Awaits! Discover a world of wisdom, inspiration,
+            and thought-provoking quotes. Whether you seek motivation, guidance,
+            or a moment of reflection, Quoterr has you covered. Explore our vast
+            collection of quotes from renowned authors, thinkers, and
+            visionaries. Let the words of wisdom inspire and uplift you. Get
+            ready to embark on a journey of enlightenment and empowerment. Start
+            your day with a dash of inspiration - Quoterr awaits!
+          </p>
+        </div>
         <Card />
-      </header>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        logo: ["Montserrat"],
+        curve: ["Pacifico"],
+        main: ["Outfit"],
+      },
+    },
+    colors: {
+      main1: "#53B8BB",
+      main2: "#055052",
+      main3: "#003638",
+      text1: "#fafafa",
+      logoQ: "#FF0000",
+    },
   },
   plugins: [],
 };
