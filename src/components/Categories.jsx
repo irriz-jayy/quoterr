7043202bf7aff8e75ff5794fce0aa308cddcf95a
irriz-jayy <jayfelix789@gmail.com>
2023-07-26 22:52:21 +0300

    some category page adjustments

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import back from "../assets/back.png";
+import Card from "./Card";
 
 function Categories() {
   return (
@@ -11,6 +13,21 @@ function Categories() {
             </p>
           </div>
         </header>
+        <div className="min-h-[90vh] border">
+          <div className="border h-96">
+            <div className="flex items-center w-full h-12 text-left text-xl p-2">
+              <span className="h-12 mr-2">
+                <a href="/">
+                  <img src={back} className="h-12" />
+                </a>
+              </span>
+              Categories
+            </div>
+            <div className="min-h-[75vh] grid grid-cols-1 gap-4 m-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+              <Card />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
